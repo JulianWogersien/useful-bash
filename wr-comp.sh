b2820d61f0de22866d626c24f7163cff4c7e2391
coolian1337 <julian.wogersien@gmail.com>
2023-01-27 15:02:19 +0100

    fixed formatting for real

--- wr-comp.sh
+++ wr-comp.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 array_contains() {
-	local array="$1[@]"
-	local seeking=$2
-	local in=1
-	for element in "${!array}"; do
-		if [[ $element == "$seeking" ]]; then
-			in=0
-			break
-		fi
-	done
-	return $in
+    local array="$1[@]"
+    local seeking=$2
+    local in=1
+    for element in "${!array}"; do
+        if [[ $element == "$seeking" ]]; then
+            in=0
+            break
+        fi
+    done
+    return $in
 }
 
 linesf1=()
@@ -21,18 +21,19 @@ file1="$1"
 file2="$2"
 
 while read -r line; do
-	linesf1+=("$line");
+    linesf1+=("$line");
 done <$file1
 
 while read -r line; do
-	linesf2+=("$line")
+    linesf2+=("$line")
 done <$file2
 
 for (( i=0; i<${#linesf2[@]}; i++ )); do
-	array_contains linesf1 "${linesf2[$i]}" && lines_out+=("${linesf2[$i]}") || continue
+    array_contains linesf1 "${linesf2[$i]}" && lines_out+=("${linesf2[$i]}") || continue
 done
+
 for (( i=0; i<${#linesf1[@]}; i++ )); do
-	array_contains linesf2 "${linesf1[$i]}" && lines_out+=("${linesf1[$i]}") || continue
+    array_contains linesf2 "${linesf1[$i]}" && lines_out+=("${linesf1[$i]}") || continue
 done
 
 echo ${lines_out[*]} >> "output.txt"
