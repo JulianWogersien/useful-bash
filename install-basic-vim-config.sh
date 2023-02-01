06f35c177b5ee27221c62a923eecb358b7f93e4c
coolian1337 <julian.wogersien@gmail.com>
2023-02-01 11:05:55 +0100

    added option to install basic vim config as root

--- install-basic-vim-config.sh
+++ install-basic-vim-config.sh
@@ -3,3 +3,8 @@ wget -P ~/ https://raw.githubusercontent.com/coolian1337/basic-vim-config/main/.
 mkdir ~/.vim
 mkdir ~/.vim/colors
 wget -P ~/.vim/colors https://raw.githubusercontent.com/coolian1337/basic-vim-config/main/.vim/colors/molokai.vim
+
+if [[ "$1" == "-r" ]]; then
+    sudo cp ~/.vimrc /root
+    sudo cp -r ~/.vim /root
+fi
